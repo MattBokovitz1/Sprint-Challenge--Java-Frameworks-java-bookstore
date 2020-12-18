92757d493ff2d3f6c4b9c8ab225f5308f06b5d7e
Matthew Bokovitz <MattBokovitz@Matthews-MBP-2.fios-router.home>
2020-12-18 13:47:48 -0500

    Complete

--- bookstore/src/test/java/com/lambdaschool/bookstore/services/BookServiceImplTest.java
+++ bookstore/src/test/java/com/lambdaschool/bookstore/services/BookServiceImplTest.java
@@ -2,10 +2,10 @@ package com.lambdaschool.bookstore.services;
 
 import com.lambdaschool.bookstore.BookstoreApplication;
 import com.lambdaschool.bookstore.exceptions.ResourceNotFoundException;
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Test;
+import com.lambdaschool.bookstore.models.Section;
+import org.junit.*;
 import org.junit.runner.RunWith;
+import org.junit.runners.MethodSorters;
 import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -13,6 +13,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = BookstoreApplication.class)
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 //**********
 // Note security is handled at the controller, hence we do not need to worry about security here!
 //**********
@@ -21,6 +22,12 @@ public class BookServiceImplTest
 
     @Autowired
     private BookService bookService;
+
+    @Autowired
+    private SectionService sectionService;
+
+    @Autowired
+    private AuthorService authorService;
 
     @Before
     public void setUp() throws
@@ -39,21 +46,26 @@ public class BookServiceImplTest
     @Test
     public void findAll()
     {
+        Assert.assertEquals(5, bookService.findAll().size());
     }
 
     @Test
     public void findBookById()
     {
+        Assert.assertEquals("Flatterland", bookService.findBookById(26).getTitle());
     }
 
     @Test(expected = ResourceNotFoundException.class)
     public void notFindBookById()
     {
+        bookService.findBookById(23);
     }
 
     @Test
     public void delete()
     {
+        bookService.delete(26);
+        Assert.assertEquals(5, bookService.findAll().size());
     }
 
     @Test
